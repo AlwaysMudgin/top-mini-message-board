9bd8bafc1276e5f63eee0d8bc8e665bc550a0abb
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2026-01-13 19:36:06 -0500

    Added start script to package.json

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/new', controller.insertMessage);
 
 app.get('/message/:id', controller.getMessageById);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
